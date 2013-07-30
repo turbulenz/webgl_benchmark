b14650bb3fcce96c7802ea2d34afb065ce195c00
David Fooks <davidfooks@turbulenz.biz>
2013-07-30 17:17:38 +0100

    Fix incorrect config settings

--- scripts/config.js
+++ scripts/config.js
@@ -6,13 +6,14 @@ function Config() {}
 
 Config.prototype =
 {
-    capturePath: 'capture/noshadows_norendertarget/',
+    capturePath: 'capture/',
     numTotalFrames: 3600,
     numFramesPerGroup: 60,
     // only works for 'norendertarget' captures
-    multisample: 1,
+    multisample: 4,
 
-    fixedFrameRate: true,
+    fixedFrameRate: false,
+
     // use getScreenshot to force the rendering to complete
     blockForRendering: false,
 
