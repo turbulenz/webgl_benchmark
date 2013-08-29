2c02739f7293061a0a320deb0474dca7447f4ad7
Ian Ballantyne <ianballantyne@turbulenz.biz>
2013-08-29 12:03:36 +0100

    Modified template to use a fixed version of capturegraphicsdevice since it is being modified faster than open source updates.

--- templates/benchmark.js
+++ templates/benchmark.js
@@ -6,14 +6,22 @@
 /*{{ javascript("scripts/configurations/offlineconfig.js") }}*/
 /*{{ javascript("scripts/configurations/localconfig.js") }}*/
 
+/*{# benchmark files #}*/
 /*{{ javascript("scripts/benchmarkapp.js") }}*/
 /*{{ javascript("scripts/loadingscreen.js") }}*/
 /*{{ javascript("scripts/playbackcontroller.js") }}*/
 /*{{ javascript("scripts/config.js") }}*/
 /*{{ javascript("scripts/querystring.js") }}*/
 
-// jslib files
-/*{{ javascript("jslib/capturegraphicsdevice.js") }}*/
+/*{# jslib files #}*/
+/*{{ javascript("jslib/services/turbulenzservices.js") }}*/
+/*{{ javascript("jslib/services/turbulenzbridge.js") }}*/
+/*{{ javascript("jslib/services/userdatamanager.js") }}*/
+/*{{ javascript("jslib/services/gamesession.js") }}*/
+/*{{ javascript("jslib/requesthandler.js") }}*/
+
+/*{# updated jslib files #}*/
+/*{{ javascript("scripts/capturegraphicsdevice.js") }}*/
 
 /*global TurbulenzEngine: false*/
 /*global BenchmarkApp: false*/
