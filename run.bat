1d5c7a17801ed5bb2e71862de319afd3123c082c
Ian Ballantyne <ianballantyne@turbulenz.biz>
2013-08-29 21:00:03 +0100

    Updated paths to config for run.bat

--- run.bat
+++ run.bat
@@ -7,20 +7,20 @@ set TARGET=%2
 if "%TARGET%" == "" goto DEFAULT_TARGET else goto CHECK_CONFIG
 
 :DEFAULT_TARGET
-if exist config\default_target.txt set /p TARGET=<config\default_target.txt
+if exist assets\config\default_target.txt set /p TARGET=<assets\config\default_target.txt
 if "%TARGET%" == "" goto EXIT_TARGET_MISSING else goto CHECK_CONFIG
 
 :CHECK_CONFIG
 if "%CONFIG%" == "" goto DEFAULT_CONFIG else goto SET_QRES_PATH
 
 :DEFAULT_CONFIG
-if exist config\default_config.txt set /p CONFIG=<config\default_config.txt
+if exist assets\config\default_config.txt set /p CONFIG=<assets\config\default_config.txt
 if "%CONFIG%" == "" goto EXIT_CONFIG_MISSING else goto SET_QRES_PATH
 
 :SET_QRES_PATH
 set QRES_PATH=external\qres\bin\win32\qres.exe
 
-if "%PYTHONPATH%" == "hj" goto SET_PYTHONPATH else goto CHECK_PYTHONPATH
+if "%PYTHONPATH%" == "" goto SET_PYTHONPATH else goto CHECK_PYTHONPATH
 
 :SET_PYTHONPATH
 set PYTHONPATH="C:\Python27\python.exe"
@@ -33,9 +33,9 @@ if not exist %PYTHONPATH% goto EXIT_PYTHON_MISSING else goto RUN
 
 %QRES_PATH% /V /S > current_resolution.txt
 
-set /p RUN_RESOLUTION=<config\default_resolution.txt
+set /p RUN_RESOLUTION=<assets\config\default_resolution.txt
 if "%RUN_RESOLUTION%" == "" goto EXIT_RESOLUTION_MISSING
-%PYTHONPATH% resolution.py --parse config\default_resolution.txt > qres_run.txt
+%PYTHONPATH% resolution.py --parse assets\config\default_resolution.txt > qres_run.txt
 %PYTHONPATH% resolution.py --parse current_resolution.txt > qres_current.txt
 set /p QRES_CURRENT_ARGS=<qres_current.txt
 set /p QRES_NEW_ARGS=<qres_run.txt
@@ -52,7 +52,7 @@ del current_resolution.txt
 goto EXIT
 
 :EXIT_RESOLUTION_MISSING
-echo Resolution is not set. Add it to config\default_resolution.txt
+echo Resolution is not set. Add it to assets\config\default_resolution.txt
 
 goto EXIT
 
@@ -61,7 +61,7 @@ echo Argument TARGET missing. Usage: run.bat CONFIG TARGET
 echo e.g.
 echo      run.bat sequence_name offline
 echo or
-echo      add TARGET to config\default_target.txt
+echo      add TARGET to assets\config\default_target.txt
 
 goto EXIT
 
@@ -70,7 +70,7 @@ echo Argument CONFIG missing. Usage: run.bat CONFIG TARGET
 echo e.g.
 echo      run.bat sequence_name offline
 echo or
-echo      add CONFIG to config\default_config.txt
+echo      add CONFIG to assets\config\default_config.txt
 goto EXIT
 
 :EXIT_PYTHON_MISSING
